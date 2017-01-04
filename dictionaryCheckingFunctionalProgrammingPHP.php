e46a07eb928bd7d448af32fb5fa877ce9c7b9a26
Dhiraj Patra <dhiraj.patra@gmail.com>
2017-01-04 11:43:55 +0530

    updated to make faster operation
    updated to make faster operation by sorted the word array

--- dictionaryCheckingFunctionalProgrammingPHP.php
+++ dictionaryCheckingFunctionalProgrammingPHP.php
@@ -4,12 +4,17 @@
 * to calculate total scores for whole sentences
 *
 */
-$time = microtime();
+//$time = microtime();
 $dictionary = array('good' => 100, 'bad' => 0, "god" => 50, "man" => 40, "is"=> 50, "wife" => 40, "country" => 70, "love" => 50, "he" => 20, "his" => 20, "for" => 10, "and" => 30);
 $sentence = "He is a very good man and love for his country and his wife is good";
 
 //$sentenceArr = explode(' ', $sentence);
 $sentenceArr = array_count_values(str_word_count($sentence, 1));
+// sort the array
+uksort($sentenceArr, function($a, $b){
+    if (strtolower($a) == strtolower($b)) return 0;
+    return (strtolower($a) < strtolower($b)) ? -1 : 1;
+});
 //print_r($sentenceArr); exit;
 $count = 0;
 /*$scores = 0;
@@ -37,7 +42,7 @@ function countScore($sentenceArr, $dictionary, &$scores)
     return true;
 }
 
-echo microtime() - $time;
+//echo microtime() - $time;
 //echo "<br>" . $scores.'  '.$count;
 countScore($sentenceArr, $dictionary, $scores);
 echo "<br>" . $scores;
